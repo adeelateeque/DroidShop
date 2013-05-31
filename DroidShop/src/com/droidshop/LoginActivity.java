0ab1b070b634cac34af6abda7009ddea212e21f8
Chong Jia Qian <jiaqian--@hotmail.com>
2013-05-31 23:21:46 +0800

    done with shopping list/cart/checkout ui

--- DroidShop/src/com/droidshop/LoginActivity.java
+++ DroidShop/src/com/droidshop/LoginActivity.java
@@ -1,23 +1,17 @@
 package com.droidshop;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class LoginActivity extends FragmentActivity implements OnClickListener {
+public class LoginActivity extends FragmentActivity {
 	
 	private FacebookFragment facebookFragment;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_login);
-	    findViewById(R.id.btnRegister).setOnClickListener(this);
+	    findViewById(R.id.sign_in_button).setOnClickListener(this);
 	    if (savedInstanceState == null) {
 	        // Add the fragment on initial activity setup
 	        facebookFragment = new FacebookFragment();
@@ -30,7 +24,6 @@ public class LoginActivity extends FragmentActivity implements OnClickListener {
 	        facebookFragment = (FacebookFragment) getSupportFragmentManager()
 	        .findFragmentById(android.R.id.content);
 	    }
-	   
 	}
 
 	@Override
@@ -40,11 +33,4 @@ public class LoginActivity extends FragmentActivity implements OnClickListener {
 		return true;
 	}
 
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-		startActivity(intent);
-		Toast.makeText(getApplicationContext(), "Register", Toast.LENGTH_LONG).show();
-	}
 }
