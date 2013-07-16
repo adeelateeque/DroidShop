58f976a8cf0c5eaf5aeab157f3d59e248861434b
Chong Jia Qian <jiaqian--@hotmail.com>
2013-07-17 03:38:55 +0800

    DONE FINALLY!!

--- DroidShop/src/main/java/com/droidshop/ui/product/receipt.java
+++ DroidShop/src/main/java/com/droidshop/ui/product/receipt.java
@@ -1,14 +1,35 @@
 package com.droidshop.ui.product;
 
-import com.droidshop.R;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Random;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
+import android.widget.TextView;
+
+import com.droidshop.R;
 
 public class receipt extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.receipt);
+
+		Bundle bundle = this.getIntent().getExtras();
+		CharSequence name = bundle.getCharSequence("name");
+		CharSequence price = bundle.getCharSequence("price");
+		String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+
+		Random randomNum = new Random();
+
+		TextView invoice = (TextView)findViewById(R.id.invoiceID);
+		invoice.setText(String.valueOf(randomNum.nextInt(1000000000)));
+		TextView itemName = (TextView)findViewById(R.id.itemDescription);
+		itemName.setText("Item: " + name);
+		TextView itemPrice = (TextView)findViewById(R.id.totalPrice);
+		itemPrice.setText("Total Price: " + price);
+		TextView date = (TextView)findViewById(R.id.dateTime);
+		date.setText(currentDateTimeString);
 	}
 }
